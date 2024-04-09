b522fd3be813550b34b687e4deae516bff4fc5df
Vítor Santos <jvitor.ti03@gmail.com>
2024-04-09 15:55:13 -0300

    Heritage

---
+++ firstProgram/src/br/com/firstProgramacao/movie/model/Title.java
@@ -0,0 +1,44 @@
+package br.com.firstProgramacao.movie.model;
+
+public class Title {
+    public String name;
+    public int debutYear;
+    public int durationMovie;
+    private int totalEvaluations;
+    private double sumOfAssessment;
+    private double mediaAssessment;
+    boolean includedInThePlan;
+
+    public void showMovieInfo(){
+        System.out.printf("br.com.firstProgramacao.movie.model.Movie name: %s.\n", this.name);
+        System.out.printf("Debut year: %d.\n", this.debutYear);
+        System.out.printf("Media evaluation: %.1f.\n", this.mediaAssessment);
+        System.out.printf("Total evaluations: %d.\n", this.totalEvaluations);
+    }
+
+    public void getMediaAssessment(){
+        this.mediaAssessment = sumOfAssessment / totalEvaluations;
+    }
+
+    public void evaluationMovie(double evaluation){
+        this.sumOfAssessment += evaluation;
+        this.totalEvaluations++;
+        getMediaAssessment();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDebutYear() {
+        return debutYear;
+    }
+
+    public void setDebutYear(int debutYear) {
+        this.debutYear = debutYear;
+    }
+}
