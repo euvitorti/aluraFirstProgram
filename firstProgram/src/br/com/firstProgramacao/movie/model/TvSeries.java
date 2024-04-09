d603e4ddb0d1c1dd914ae83ad4f7115eb500c3ab
Vítor Santos <jvitor.ti03@gmail.com>
2024-04-09 16:02:27 -0300

    Class series

---
+++ firstProgram/src/br/com/firstProgramacao/movie/model/TvSeries.java
@@ -0,0 +1,40 @@
+package br.com.firstProgramacao.movie.model;
+
+public class TvSeries extends Title{
+    private int season;
+    private int episodeSeason;
+    private int episodeMinutes;
+    private boolean active;
+
+    public int getSeason() {
+        return season;
+    }
+
+    public void setSeason(int season) {
+        this.season = season;
+    }
+
+    public int getEpisodeSeason() {
+        return episodeSeason;
+    }
+
+    public void setEpisodeSeason(int episodeSeason) {
+        this.episodeSeason = episodeSeason;
+    }
+
+    public int getEpisodeMinutes() {
+        return episodeMinutes;
+    }
+
+    public void setEpisodeMinutes(int episodeMinutes) {
+        this.episodeMinutes = episodeMinutes;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
