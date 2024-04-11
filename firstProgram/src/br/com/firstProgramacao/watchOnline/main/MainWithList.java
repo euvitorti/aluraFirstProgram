81cb65a64b37be4d0e487cafc209fe3c40660602
Vítor Santos <jvitor.ti03@gmail.com>
2024-04-11 16:02:58 -0300

    MORE INFORMATIONS
    PONTEIROS E ALGUMAS FORMA DO CÓDIGO LEGADO

--- firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainWithList.java
+++ firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainWithList.java
@@ -10,8 +10,16 @@ public class MainWithList {
     public static void main(String[] args) {
         // Reference type
         Movie movieInfo = new Movie("Interstellar", 2014);
+        movieInfo.evaluationMovie(7);
+        movieInfo.evaluationMovie(9);
 
         Movie movie = new Movie("Bot",2008);
+        movie.evaluationMovie(2);
+        movie.evaluationMovie(9);
+
+        // ESTÁ SE REFERENCIANDOO AO MESMO OBJETO, ASSUNTO DE PONTEIROS
+        // POIS NEM DEU UM NEW
+        Movie bot = movie;
 
         TvSeries series = new TvSeries("It's Ok Not Be Okay", 2020);
 
@@ -23,8 +31,13 @@ public class MainWithList {
         // FOREACH
         for (Title title: listPrograms){
             System.out.println(title.getName());
-            Movie movieTwo = (Movie) title;
-            System.out.println(movieTwo.getClassification());
+
+            // FORMA QUE POSSO ENCONTRAR EM ALGUNS CÓDIGOS LEGADOS
+            if (title instanceof Movie movieTwo){
+//                Movie movieTwo = (Movie) title;
+                System.out.println(movieTwo.getClassification());
+            }
+//            Movie movieTwo = (Movie) title;
         }
         
     }
