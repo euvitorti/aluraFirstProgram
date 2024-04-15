190baebfe75bfe7f0d72770107ca925297f4b762
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-04-15 14:37:17 -0300

    Method get
    Método para fazer a requisição para a api.

--- firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainSearch.java
+++ firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainSearch.java
@@ -1,7 +1,25 @@
 package br.com.firstProgramacao.watchOnline.main;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+
+
 public class MainSearch {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // Para fazer a requisição (GET)
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?paramsx&apikey=YourKey"))
+                .build();
+
+        // RECEBENDO A RESPOSTA
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
     }
 }
