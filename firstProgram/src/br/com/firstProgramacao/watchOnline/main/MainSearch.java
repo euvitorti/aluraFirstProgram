fef2a4305cef6631cff07b97403869603f25d0d1
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-04-17 15:48:41 -0300

    Salvando arquivos em json
    Mudança do escopo para as variáveis.

--- firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainSearch.java
+++ firstProgram/src/br/com/firstProgramacao/watchOnline/main/MainSearch.java
@@ -13,6 +13,8 @@ import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -20,50 +22,70 @@ public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Movie or series name: ");
-        String search = scanner.nextLine();
+        List<Title> titleList =  new ArrayList<>();
 
-        String address = "https://www.omdbapi.com/?t=" + search.replace(" ","+") + "&apikey=cea70f0f";
+        String search = "";
 
-        try{
-            // Para fazer a requisição (GET)
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(address))
-                    .build();
+        // AQUI ESTÁ PEGANDO O PARÃO DE NOMENCÇATURA
+        // O JSON VEM EM UPPERCASE
+        // E VAI SE RESPONSABILIZAR POR IGNORAR
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
-            // RECEBENDO A RESPOSTA
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        while (!search.equalsIgnoreCase("exit")) {
 
-            String json = response.body();
+            System.out.println("Movie or series name: ");
+            search = scanner.nextLine();
+
+            if (search.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            String address = "https://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=cea70f0f";
+
+            try {
+                // Para fazer a requisição (GET)
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(address))
+                        .build();
+
+                // RECEBENDO A RESPOSTA
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
 //        System.out.println(json);
 
-            // AQUI ESTÁ PEGANDO O PARÃO DE NOMENCÇATURA
-            // O JSON VEM EM UPPERCASE
-            // E VAI SE RESPONSABILIZAR POR IGNORAR
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-            OmdbTitle omdbTitle = gson.fromJson(json, OmdbTitle.class);
+                OmdbTitle omdbTitle = gson.fromJson(json, OmdbTitle.class);
 //        System.out.println(omdbTitle);
 
-            Title title = new Title(omdbTitle);
-            System.out.println(title);
+                Title title = new Title(omdbTitle);
+                System.out.println(title);
 
-            // ESCREVENDO EM UM ARQUIVO OS FILMES QUE FORAM PESQUISADOS
-            FileWriter fileWriter = new FileWriter("movie.txt");
-            fileWriter.write(title.toString());
-            fileWriter.close();
-        } catch (NumberFormatException e) {
+                titleList.add(title);
 
-            System.out.println("ERROR");
-            System.out.println(e.getMessage());
+//                // ESCREVENDO EM UM ARQUIVO OS FILMES QUE FORAM PESQUISADOS
+//                FileWriter fileWriter = new FileWriter("movie.txt");
+//                fileWriter.write(title.toString());
+//                fileWriter.close();
+            } catch (NumberFormatException e) {
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("I ALA ESCREVEU ERRADO");
-        } catch (InvalidYearException e){
-            System.out.println(e.getMessage());
+                System.out.println("ERROR");
+                System.out.println(e.getMessage());
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("I ALA ESCREVEU ERRADO");
+            } catch (InvalidYearException e) {
+                System.out.println(e.getMessage());
+            }
         }
+
+        FileWriter fileWriter = new FileWriter("movies.json");
+        fileWriter.write(gson.toJson(titleList));
+        fileWriter.close();
+
     }
 }
